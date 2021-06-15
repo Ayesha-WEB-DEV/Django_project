e59fa63c89e2458376fa35e8719119a653ac9073
Ayesha-WEB-DEV <ayeshaislamqadri@gmail.com>
2021-06-15 12:57:33 +0500

    First try with dictionaries

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Ayesha',
+        'title': 'First blog try',
+        'content': 'Lab honi thi',
+        'date_posted': '15 june, 2021'
+    },
+    {
+        'author': 'HAHAHAHA',
+        'title': 'Second blog try',
+        'content': 'Nae huwi',
+        'date_posted': '15 june, 2021'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
